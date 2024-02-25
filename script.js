76432ed8ec64b3d2730528d1819ed656fe21f499
GargDivanshu <divanshu.garg113@gmail.com>
2024-02-26 01:02:17 +0530

    sol push

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
 
-// Write your code here and print the output using alert function
+// Check that the input is numerical
+if (input1 === "" || input2 === "" || isNaN(input1) || isNaN(input2)) {
+  alert("Invalid input. Please enter a valid number.");
+} else {
+  // Add the two input numbers
+  let sum = Number(input1) + Number(input2);
+
+  // Display the sum to the user
+  alert(`The sum of ${input1} and ${input2} is ${sum}.`);
+}
